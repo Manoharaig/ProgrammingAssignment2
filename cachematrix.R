e6836f0e54226dd21bb85e5aa339023621a3b758
Manoharaig <manohar_gkvk02@rediffmail.com>
2016-05-15 22:41:58 +0530

    Updated the Program assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inv <- NULL                     # Initializing the inverse matrix to NULL
+  set <- function(y) {          # function to set the a new value for the underlying matrix, We use <<- operator to set the value of x
+    x <<- y                     # and inverse because we want to modify x and inverse defined in the closed environment        
+    inv <<- NULL
+  }
+  get <- function() x           # getting the matrix
+  setInverse <- function(inverse) inv <<- inverse       # set the inverse of matrix
+  getInverse <- function() inv                          # Get the inverse of matrix
+  list(set = set,                                      
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
@@ -12,4 +23,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+          if (!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+          }
+          mat <- x$get()
+          inv <- solve(mat, ...)
+          x$setInverse(inv)
+          inv
+        }
+
 }
